d32ef6d6a249567a5f9275f7fc065a68635a9d5f
DucCao <condorhero01@gmail.com>
2012-12-28 16:30:27 +0700

    make the notification can not be deleted

--- src/com/condorhero89/nightguardian/util/NotificationUtil.java
+++ src/com/condorhero89/nightguardian/util/NotificationUtil.java
@@ -15,6 +15,7 @@ public class NotificationUtil {
 	public static void showNotification(Context context) {
 		NotificationManager notifManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 		Notification note = new Notification(R.drawable.ic_launcher, "NightGuardian", System.currentTimeMillis());
+		note.flags |= Notification.FLAG_NO_CLEAR ;
 
 		PendingIntent intent = PendingIntent.getActivity(context, 0, new Intent(), 0);
 		note.setLatestEventInfo(context, "NightGuardian", "NightGuardian's running", intent);
