e720a69827ade7a7c14d88d0029a76d7ab7efca3
Duc Cao <condorhero01@gmail.com>
2013-01-15 22:50:07 +0700

    finish cancel timer feature

--- src/com/condorhero89/nightguardian/util/TimerUtil.java
+++ src/com/condorhero89/nightguardian/util/TimerUtil.java
@@ -6,12 +6,16 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 import com.condorhero89.nightguardian.service.NightGuardianService;
 import com.condorhero89.nightguardian.service.NightGuardianStopperService;
 
 public class TimerUtil {
+	private static final String TAG = TimerUtil.class.getSimpleName();
     private static final int ONE_DAY = 1000 * 60 * 60 * 24;
+    private static final int REQUEST_CODE_START = 0;
+    private static final int REQUEST_CODE_STOP = 1;
 
     public static boolean startTimer(Context context) {
     	boolean isServiceRunning = ServiceUtil.isServiceRunning(context, NightGuardianService.class.getSimpleName());
@@ -26,7 +30,7 @@ public class TimerUtil {
         calendar.set(Calendar.SECOND, 0);
         
         Intent myIntent = new Intent(context, NightGuardianService.class);
-        PendingIntent pendingIntent = PendingIntent.getService(context, 0, myIntent, 0);
+        PendingIntent pendingIntent = PendingIntent.getService(context, REQUEST_CODE_START, myIntent, 0);
 
         AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
         alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
@@ -38,13 +42,13 @@ public class TimerUtil {
     public static void stopTimer(Context context) {
         Calendar calendar = Calendar.getInstance();
         calendar.setTimeInMillis(System.currentTimeMillis());
-        calendar.add(Calendar.DAY_OF_YEAR, 1);
+        calendar.add(Calendar.DAY_OF_YEAR, 1);	// tomorrow = today + 1
         calendar.set(Calendar.HOUR_OF_DAY, NightGuardianPreference.getStopTime(context));
         calendar.set(Calendar.MINUTE, NightGuardianPreference.getStopMinute(context));
         calendar.set(Calendar.SECOND, 0);
         
         Intent myIntent = new Intent(context, NightGuardianStopperService.class);
-        PendingIntent pendingIntent = PendingIntent.getService(context, 1, myIntent, 0);
+        PendingIntent pendingIntent = PendingIntent.getService(context, REQUEST_CODE_STOP, myIntent, 0);
 
         AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
         alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), ONE_DAY, pendingIntent);
@@ -53,12 +57,20 @@ public class TimerUtil {
     public static void cancel(Context context) {
     	AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
     	
-    	Intent myIntent = new Intent(context, NightGuardianService.class);
-        PendingIntent pendingIntent = PendingIntent.getService(context, 0, myIntent, 0);
-        alarmManager.cancel(pendingIntent);
-        
-        myIntent = new Intent(context, NightGuardianStopperService.class);
-        pendingIntent = PendingIntent.getService(context, 1, myIntent, 0);
-        alarmManager.cancel(pendingIntent);
+    	boolean isNightGuardianServiceRunning = ServiceUtil.isServiceRunning(context, NightGuardianService.class.getSimpleName());
+    	if (isNightGuardianServiceRunning) {
+    		Log.i(TAG, "cancel timer for NightGuardianService");
+    		Intent myIntent = new Intent(context, NightGuardianService.class);
+    		PendingIntent pendingIntent = PendingIntent.getService(context, REQUEST_CODE_START, myIntent, 0);
+    		alarmManager.cancel(pendingIntent);
+    		
+    		// also stop the service
+    		context.startService(new Intent(context, NightGuardianStopperService.class));
+    		
+    		Log.i(TAG, "cancel timer for NightGuardianStopperService");
+    		myIntent = new Intent(context, NightGuardianStopperService.class);
+    		pendingIntent = PendingIntent.getService(context, REQUEST_CODE_STOP, myIntent, 0);
+    		alarmManager.cancel(pendingIntent);
+    	}
     }
 }
